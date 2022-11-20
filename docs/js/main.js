1b1ca38baa23ba46cd875ccaf5e72ee2ec37b1aa
kenkenpa198 <kenkenpa198@gmail.com>
2022-11-21 07:29:31 +0900

    [fix]Colorbox の展開時にタイトルが表示されなくなっていた問題を修正

--- docs/js/main.js
+++ docs/js/main.js
@@ -87,7 +87,7 @@ $(function () {
                 returnFocus: false, // モーダルを閉じたときにそのモーダルのトリガーとなったリンクにフォーカスを戻さない  True の場合 iOS Safari だと青い選択が表示されるため
                 reposition: false,  // ウインドウがリサイズされたときにモーダルの位置を変更しない True だと iOS で拡大時にクラッシュするため
                 title: function () {
-                    return $(this).children('.inner').html();
+                    return $(this).find('.inner').html();
                 }
             })
 
